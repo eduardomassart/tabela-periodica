235d1e72aa9d44afc2455b4fe2b521f3195673b1
Eduardo Bartichoto Massare <eduardobarti@gmail.com>
2024-02-24 11:50:43 -0300

    Import API e criação html com div e classes

---
+++ index.js
@@ -0,0 +1,17 @@
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		let elementos = JSON.parse(this.responseText);
+        console.log(elementos);;
+	}
+});
+
+xhr.open('GET', 'https://periodictable.p.rapidapi.com/');
+xhr.setRequestHeader('X-RapidAPI-Key', '85dc2bc749msh4834fe2cd3cbf13p11f8f4jsn2a526925af1a');
+xhr.setRequestHeader('X-RapidAPI-Host', 'periodictable.p.rapidapi.com');
+
+xhr.send(data);
